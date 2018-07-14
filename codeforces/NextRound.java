76dbcafe79aab1a655e2ee3a684f5e5519696537
Athi101 <aathira2017@gmail.com>
2018-07-14 11:19:02 +0530

    java problems

---
+++ codeforces/NextRound.java
@@ -0,0 +1,23 @@
+package codeforces;
+
+import java.util.Scanner;
+
+public class NextRound {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int [] arr = new int[n];
+		int c = 0;
+		for(int i = 0 ; i<n ; i++) {
+			arr[i] = sc.nextInt();
+			if(arr[i]>=arr[k-1] && arr[i]!=0) {
+				c=c+1;
+			}
+			
+		}
+		System.out.println(c);
+		sc.close();
+		
+	}
+}
---
+++ codeforces/Team.java
@@ -0,0 +1,27 @@
+package codeforces;
+
+import java.util.Scanner;
+
+public class Team {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int count=0;
+		for(int j = 0 ; j<n ; j++) {
+			int d = 0;
+			for (int k = 0 ; k<3; k++) {
+				int h = sc.nextInt();
+				if(h==1) {
+					d=d+1;
+				}
+				
+			}
+			if(d>=2) {
+				count = count + 1;
+			}
+		}
+		System.out.println(count);
+		sc.close();
+	}
+
+}
